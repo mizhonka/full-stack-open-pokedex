63a5cfbd9a541e26ed102aafd2fb7e3e255d2799
mizhonka <116881384+mizhonka@users.noreply.github.com>
2024-04-23 15:05:44 +0300

    fix health check (11.12)

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/health', (_req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
